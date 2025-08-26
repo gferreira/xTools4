ef9f34e76eb6481297c3a139dfccf9526535db0c
gferreira <gustavo@hipertipo.com>
2025-08-26 20:29:59 -0300

    fixing toc header links

--- docs/js/toc.js
+++ docs/js/toc.js
@@ -8,18 +8,18 @@ $(function(){
   });
 });
 // add header links
-// $(document).ready(function () {
-//   $("h2, h3, h4, h5, h6").each(function() {
-//     el = $(this)
-//     id = el.attr('id')
-//     icon = '<span></span>'
-//     if (id) {
-//       el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
-//     }
-//   })
-//   // initialize tooltips
-//   var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-//   var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-//     return new bootstrap.Tooltip(tooltipTriggerEl)
-//   })
-// })
+$(document).ready(function () {
+  $("h2, h3, h4, h5, h6").each(function() {
+    el = $(this)
+    id = el.attr('id')
+    icon = '<span></span>'
+    if (id) {
+      el.append($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+    }
+  })
+  // initialize tooltips
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+})
