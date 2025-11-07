fb4e8552c0bb77bbeaf0b090bb4d366a2095b56e
gferreira <gustavo@hipertipo.com>
2025-11-07 20:50:42 -0300

    adding extra unicode entry for /Germandbls, adding swap glyphs to menu, working on BlendsPreview (WIP)

---
+++ Lib/menus/font/swap-glyphs.py
@@ -0,0 +1,10 @@
+from xTools4.modules.fontutils import swapGlyphs
+
+font = CurrentFont()
+
+if len(font.selectedGlyphNames) != 2:
+    print('please select two glyphs to swap')
+
+else:
+    glyphName1, glyphName2 = font.selectedGlyphNames
+    swapGlyphs(font, glyphName1, glyphName2)
